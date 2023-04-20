a0948606052f49b3d1298495768ddae4cd47e383
Joseph Calderon <>
2023-04-20 17:30:31 -0600

    Creacion de la Api, Creacion DA, Entities

---
+++ Entities-Manejo-Documentos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_Manejo_Documentos
+{
+    public class Class1
+    {
+    }
+}
